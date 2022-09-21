379e5a0aeaf0c4236e4391ce2cf91be7195cfdef
Mauricio <Siumauricio@unitec.edu>
2022-09-20 20:43:25 -0600

    feat: add companies dropdown

---
+++ components/sidebar/companies-dropdown.tsx
@@ -0,0 +1,88 @@
+import {Dropdown, Navbar, Text} from '@nextui-org/react';
+import React from 'react';
+import {AcmeIcon} from '../icons/acme-icon';
+import {AcmeLogo} from '../icons/acmelogo';
+import {BottomIcon} from '../icons/bottom-icon';
+import {Box} from '../styles/box';
+import {Flex} from '../styles/flex';
+
+export const CompaniesDropdown = () => {
+   return (
+      <Dropdown placement="bottom-right" borderWeight={'extrabold'}>
+         <Dropdown.Trigger css={{cursor: 'pointer'}}>
+            <Box>
+               <Flex align={'center'} css={{gap: '$7'}}>
+                  <Box>
+                     <Text
+                        h3
+                        size={'$xl'}
+                        weight={'medium'}
+                        css={{
+                           m: 0,
+                           color: '$accents9',
+                           lineHeight: '$lg',
+                           mb: '-$5',
+                        }}
+                     >
+                        Acme Co.{' '}
+                     </Text>
+                     <Text
+                        span
+                        weight={'medium'}
+                        size={'$xs'}
+                        css={{color: '$accents8'}}
+                     >
+                        Palo Alto, CA
+                     </Text>
+                  </Box>
+                  <BottomIcon />
+               </Flex>
+            </Box>
+         </Dropdown.Trigger>
+         <Dropdown.Menu
+            aria-label="Avatar Actions"
+            css={{
+               '$$dropdownMenuWidth': '340px',
+               '$$dropdownItemHeight': '60px',
+               '& .nextui-dropdown-item': {
+                  'py': '$2',
+                  // dropdown item left icon
+                  'svg': {
+                     color: '$secondary',
+                     mr: '$4',
+                  },
+                  // dropdown item title
+                  '& .nextui-dropdown-item-content': {
+                     w: '100%',
+                     fontWeight: '$semibold',
+                  },
+               },
+            }}
+         >
+            <Dropdown.Section title="Companies">
+               <Dropdown.Item
+                  key="1"
+                  icon={<AcmeIcon />}
+                  description="San Fransico, CA"
+               >
+                  Facebook
+               </Dropdown.Item>
+               <Dropdown.Item
+                  key="2"
+                  icon={<AcmeLogo />}
+                  description="Austin, Tx"
+               >
+                  Instagram
+               </Dropdown.Item>
+               <Dropdown.Item
+                  key="3"
+                  icon={<AcmeIcon />}
+                  description="Brooklyn, NY"
+               >
+                  Twitter
+               </Dropdown.Item>
+            </Dropdown.Section>
+         </Dropdown.Menu>
+      </Dropdown>
+   );
+};
