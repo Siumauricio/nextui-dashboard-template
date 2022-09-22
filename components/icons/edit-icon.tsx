511e959d5b4f24fca9b1cc8c42fccc6efd246621
Mauricio <Siumauricio@unitec.edu>
2022-09-22 00:37:06 -0600

    feat: add icon

---
+++ components/icons/edit-icon.tsx
@@ -0,0 +1,44 @@
+interface Props {
+   size?: number;
+   fill?: string;
+   width?: number;
+   height?: number;
+}
+
+export const EditIcon = ({fill, size, height, width, ...props}: Props) => {
+   return (
+      <svg
+         width={size || width || 24}
+         height={size || height || 24}
+         viewBox="0 0 20 20"
+         fill="none"
+         xmlns="http://www.w3.org/2000/svg"
+         {...props}
+      >
+         <path
+            d="M11.05 3.00002L4.20835 10.2417C3.95002 10.5167 3.70002 11.0584 3.65002 11.4334L3.34169 14.1334C3.23335 15.1084 3.93335 15.775 4.90002 15.6084L7.58335 15.15C7.95835 15.0834 8.48335 14.8084 8.74168 14.525L15.5834 7.28335C16.7667 6.03335 17.3 4.60835 15.4583 2.86668C13.625 1.14168 12.2334 1.75002 11.05 3.00002Z"
+            stroke={fill}
+            strokeWidth={1.5}
+            strokeMiterlimit={10}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+         <path
+            d="M9.90833 4.20831C10.2667 6.50831 12.1333 8.26665 14.45 8.49998"
+            stroke={fill}
+            strokeWidth={1.5}
+            strokeMiterlimit={10}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+         <path
+            d="M2.5 18.3333H17.5"
+            stroke={fill}
+            strokeWidth={1.5}
+            strokeMiterlimit={10}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+         />
+      </svg>
+   );
+};
