24e6c2c4f70059b2aa5c3a6cd268706818c92c95
Mauricio <Siumauricio@unitec.edu>
2022-09-22 00:37:45 -0600

    refactor: add wrapper for entire app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {createTheme, NextUIProvider} from '@nextui-org/react';
 import {ThemeProvider as NextThemesProvider} from 'next-themes';
+import {Layout} from '../components/layout/layout';
 
 const lightTheme = createTheme({
    type: 'light',
@@ -28,7 +29,9 @@ function MyApp({Component, pageProps}: AppProps) {
          }}
       >
          <NextUIProvider>
-            <Component {...pageProps} />
+            <Layout>
+               <Component {...pageProps} />
+            </Layout>
          </NextUIProvider>
       </NextThemesProvider>
    );
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,7 @@ import {Layout} from '../components/layout/layout';
 import {Content} from '../components/navbar/content';
 
 const Home: NextPage = () => {
-   return (
-      <Layout>
-         <Head>
-            <title>Dashboard | Next UI</title>
-            <meta name="description" content="Next UI Dashboard" />
-         </Head>
-         <Content />
-      </Layout>
-   );
+   return <Content />;
 };
 
 export default Home;
