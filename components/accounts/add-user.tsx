c7b6d6d1409e02b4c60903b82492e0776e05e025
Mauricio <Siumauricio@unitec.edu>
2022-09-22 00:37:16 -0600

    feat: add modal for add user

---
+++ components/accounts/add-user.tsx
@@ -0,0 +1,132 @@
+import {
+   Button,
+   Checkbox,
+   Divider,
+   Input,
+   Modal,
+   Row,
+   Text,
+} from '@nextui-org/react';
+import React from 'react';
+import {Flex} from '../styles/flex';
+
+export const AddUser = () => {
+   const [visible, setVisible] = React.useState(false);
+   const handler = () => setVisible(true);
+
+   const closeHandler = () => {
+      setVisible(false);
+      console.log('closed');
+   };
+
+   return (
+      <div>
+         <Button auto shadow onClick={handler}>
+            Add User
+         </Button>
+         <Modal
+            closeButton
+            aria-labelledby="modal-title"
+            width="600px"
+            open={visible}
+            onClose={closeHandler}
+         >
+            <Modal.Header css={{justifyContent: 'start'}}>
+               <Text id="modal-title" h4>
+                  Add new user
+               </Text>
+            </Modal.Header>
+            <Divider css={{my: '$5'}} />
+            <Modal.Body css={{py: '$10'}}>
+               <Flex
+                  direction={'column'}
+                  css={{
+                     'flexWrap': 'wrap',
+                     'gap': '$8',
+                     '@lg': {flexWrap: 'nowrap', gap: '$12'},
+                  }}
+               >
+                  <Flex
+                     css={{
+                        'gap': '$10',
+                        'flexWrap': 'wrap',
+                        '@lg': {flexWrap: 'nowrap'},
+                     }}
+                  >
+                     <Input
+                        label="First Name"
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="First Name"
+                     />
+                     <Input
+                        label="Last Name"
+                        clearable
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="Last Name"
+                     />
+                  </Flex>
+
+                  <Flex
+                     css={{
+                        'gap': '$10',
+                        'flexWrap': 'wrap',
+                        '@lg': {flexWrap: 'nowrap'},
+                     }}
+                  >
+                     <Input
+                        label="Email"
+                        clearable
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="Email"
+                     />
+                     <Input
+                        label="Phone Number"
+                        clearable
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="Phone Number"
+                     />
+                  </Flex>
+                  <Flex
+                     css={{
+                        'gap': '$10',
+                        'flexWrap': 'wrap',
+                        '@lg': {flexWrap: 'nowrap'},
+                     }}
+                  >
+                     <Input
+                        label="Department"
+                        clearable
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="Department"
+                     />
+                     <Input
+                        label="Company"
+                        clearable
+                        bordered
+                        fullWidth
+                        size="lg"
+                        placeholder="Company"
+                     />
+                  </Flex>
+               </Flex>
+            </Modal.Body>
+            <Divider css={{my: '$5'}} />
+            <Modal.Footer>
+               <Button auto onClick={closeHandler}>
+                  Add User
+               </Button>
+            </Modal.Footer>
+         </Modal>
+      </div>
+   );
+};
