f9777a369bebd7bc80e9a3929cf79609ca573691
Mauricio <Siumauricio@unitec.edu>
2022-09-22 22:52:21 -0600

    moved

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {DeleteIcon} from '../icons/table/delete-icon';
 import {EditIcon} from '../icons/table/edit-icon';
 import {EyeIcon} from '../icons/table/eye-icon';
+import {Box} from '../styles/box';
 import {columns, users} from './data';
 import {IconButton, StyledBadge} from './table.styled';
 
@@ -86,44 +87,52 @@ export const TableWrapper = () => {
       }
    };
    return (
-      <Table
-         aria-label="Example table with custom cells"
+      <Box
          css={{
-            height: 'auto',
-            minWidth: '100%',
-            boxShadow: 'none',
-            width: '100%',
-            px: 0,
+            '& .nextui-table-container': {
+               boxShadow: 'none',
+            },
          }}
-         selectionMode="multiple"
       >
-         <Table.Header columns={columns}>
-            {(column) => (
-               <Table.Column
-                  key={column.uid}
-                  hideHeader={column.uid === 'actions'}
-                  align={column.uid === 'actions' ? 'center' : 'start'}
-               >
-                  {column.name}
-               </Table.Column>
-            )}
-         </Table.Header>
-         <Table.Body items={users}>
-            {(item) => (
-               <Table.Row>
-                  {(columnKey) => (
-                     <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>
-                  )}
-               </Table.Row>
-            )}
-         </Table.Body>
-         <Table.Pagination
-            shadow
-            noMargin
-            align="center"
-            rowsPerPage={8}
-            onPageChange={(page) => console.log({page})}
-         />
-      </Table>
+         <Table
+            aria-label="Example table with custom cells"
+            css={{
+               height: 'auto',
+               minWidth: '100%',
+               boxShadow: 'none',
+               width: '100%',
+               px: 0,
+            }}
+            selectionMode="multiple"
+         >
+            <Table.Header columns={columns}>
+               {(column) => (
+                  <Table.Column
+                     key={column.uid}
+                     hideHeader={column.uid === 'actions'}
+                     align={column.uid === 'actions' ? 'center' : 'start'}
+                  >
+                     {column.name}
+                  </Table.Column>
+               )}
+            </Table.Header>
+            <Table.Body items={users}>
+               {(item) => (
+                  <Table.Row>
+                     {(columnKey) => (
+                        <Table.Cell>{renderCell(item, columnKey)}</Table.Cell>
+                     )}
+                  </Table.Row>
+               )}
+            </Table.Body>
+            <Table.Pagination
+               shadow
+               noMargin
+               align="center"
+               rowsPerPage={8}
+               onPageChange={(page) => console.log({page})}
+            />
+         </Table>
+      </Box>
    );
 };
