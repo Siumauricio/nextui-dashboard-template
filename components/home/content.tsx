7155e377571586c31f8e6f3ff0202676e625234c
Mehmet Kose <mehmetkosedev@gmail.com>
2023-12-09 00:29:46 +0000

    add padding content area

--- components/home/content.tsx
+++ components/home/content.tsx
@@ -17,9 +17,9 @@ const Chart = dynamic(
 );
 
 export const Content = () => (
-  <div className=" h-full">
-    <div className="flex justify-center gap-4 xl:gap-12 pt-3 px-4 lg:px-0  flex-wrap xl:flex-nowrap sm:pt-10 max-w-[90rem] mx-auto w-full">
-      <div className="mt-6  gap-6 flex flex-col w-full">
+  <div className="h-full lg:px-6">
+    <div className="flex justify-center gap-4 xl:gap-6 pt-3 px-4 lg:px-0  flex-wrap xl:flex-nowrap sm:pt-10 max-w-[90rem] mx-auto w-full">
+      <div className="mt-6 gap-6 flex flex-col w-full">
         {/* Card Section Top */}
         <div className="flex flex-col gap-2">
           <h3 className="text-xl font-semibold">Available Balance</h3>
