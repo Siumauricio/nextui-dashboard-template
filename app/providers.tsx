93c31e38973066d43aed8ddf5e544bef78fc8151
Bruno Kraychete da Costa <1355373+brunokc@users.noreply.github.com>
2024-01-07 01:03:28 -0800

    Fix build
    Removed 'navigate' attribute from NextUIProvider even though it came
    from NextUI's sample code.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,6 @@
 "use client";
-
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
-import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { Layout } from "../components/layout/layout";
@@ -13,10 +11,8 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-  const router = useRouter();
-
   return (
-    <NextUIProvider navigate={router.push}>
+    <NextUIProvider>
       <NextThemesProvider defaultTheme="system" attribute="class" {...themeProps}>
         <Layout>
           {children}
