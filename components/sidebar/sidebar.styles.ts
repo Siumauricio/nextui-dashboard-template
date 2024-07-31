b22875b79e84751f1307e591f0859962cb402e1b
Leo Lin <4258leo@gmail.com>
2024-07-31 12:29:30 +0800

    fix: overlap between the sidebar and the navigation bar on mobile view

--- components/sidebar/sidebar.styles.ts
+++ components/sidebar/sidebar.styles.ts
@@ -5,7 +5,7 @@ export const SidebarWrapper = tv({
 
   variants: {
     collapsed: {
-      true: "translate-x-0 ml-0 [display:inherit]",
+      true: "translate-x-0 ml-0 pt-20 [display:inherit]",
     },
   },
   // ""
